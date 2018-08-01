f66a8c1fe2c0fd32416ad3648875925123da3749
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-01 18:44:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function fetchBooks() {
  fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => console.log(json));
+  
+}
 
 function renderBooks(json) {
   const main = document.querySelector('main')
